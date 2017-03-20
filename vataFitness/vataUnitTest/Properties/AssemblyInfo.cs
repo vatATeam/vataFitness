aa32384b6993f308c0a92f2f62df06626cb5499f
Чепченко Константин <avelkon@aol.com>
2017-03-20 18:44:19 +0300

    RenameProjects

--- vataFitness/vataUnitTest/Properties/AssemblyInfo.cs
+++ vataFitness/vataUnitTest/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("vataUnitTest")]
+[assembly: AssemblyTitle("vataFitnessUnitTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("vataUnitTest")]
+[assembly: AssemblyProduct("vataFitnessUnitTest")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- vataFitness/vataUnitTest/UnitTest1.cs
+++ vataFitness/vataUnitTest/UnitTest1.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace vataUnitTest
+namespace vataFitnessUnitTest
 {
     [TestClass]
     public class UnitTest1
