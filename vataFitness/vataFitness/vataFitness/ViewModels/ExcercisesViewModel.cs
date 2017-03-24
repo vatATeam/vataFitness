a031781773634341375115c27ca6c8af3c0c50f2
Alex Anisimov <alex.anisimoff@gmail.com>
2017-03-24 18:33:27 +0300

    added UI pages for excercises< complexes, trainings

---
+++ vataFitness/vataFitness/vataFitness/ViewModels/ExcercisesViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using vataFitness.Helpers;
+using vataFitness.Models;
+using vataFitness.Views;
+using Xamarin.Forms;
+
+namespace vataFitness.ViewModels
+{
+    class ExcercisesViewModel : BaseViewModel
+    {
+        public ObservableRangeCollection<Item> Items { get; set; }
+        public Command LoadItemsCommand { get; set; }
+
+        public ExcercisesViewModel()
+        {
+            Title = "Browse";
+            Items = new ObservableRangeCollection<Item>();
+            LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
+
+            MessagingCenter.Subscribe<NewItemPage, Item>(this, "AddItem", async (obj, item) =>
+            {
+                var _item = item as Item;
+                Items.Add(_item);
+                await DataStore.AddItemAsync(_item);
+            });
+        }
+
+        async Task ExecuteLoadItemsCommand()
+        {
+            if (IsBusy)
+                return;
+
+            IsBusy = true;
+
+            try
+            {
+                Items.Clear();
+                var items = new ObservableCollection<Item>(new []{new Item(){Text = "Бёрпи", Description = "Тут написано как его делать"}, new Item(){Text = "Отжимание", Description = "Тут написано как делать оотжимания" },  });//await DataStore.GetItemsAsync(true);
+                Items.ReplaceRange(items);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                MessagingCenter.Send(new MessagingCenterAlert
+                {
+                    Title = "Error",
+                    Message = "Unable to load items.",
+                    Cancel = "OK"
+                }, "message");
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+    }
+}
---
+++ vataFitness/vataFitness/vataFitness/Views/ExcercisesPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vataFitness.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace vataFitness.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ExcercisesPage : ContentPage
+	{
+	    private ExcercisesViewModel _viewModel;
+
+	    public ExcercisesPage ()
+		{
+			InitializeComponent ();
+            BindingContext = _viewModel = new ExcercisesViewModel();
+        }
+
+	    private void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+	    {
+	        
+	    }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (_viewModel.Items.Count == 0)
+                _viewModel.LoadItemsCommand.Execute(null);
+        }
+    }
+
+}
